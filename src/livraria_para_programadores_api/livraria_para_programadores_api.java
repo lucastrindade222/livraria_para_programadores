6d917c69de05943759362501b69319099f833c4c
lucastrindade222 <55842839+lucastrindade222@users.noreply.github.com>
2020-11-16 22:08:57 -0300

    new projeto

---
+++ src/livraria_para_programadores_api/livraria_para_programadores_api.java
@@ -0,0 +1,10 @@
+package livraria_para_programadores_api;
+
+public class livraria_para_programadores_api {
+
+	public static void main(String[] args) {
+		System.out.println("Bom dia meu bom");
+
+	}
+
+}
